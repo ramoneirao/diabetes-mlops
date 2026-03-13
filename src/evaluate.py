1c8d39f4140a488d1f6f3bd2e2937d2c3e2984af
ramoneirao <ramonneirao@gmail.com>
2026-03-13 01:01:22 -0300

    final changes

---
+++ src/evaluate.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import pickle
+from sklearn.metrics import accuracy_score
+import yaml
+import os
+import mlflow
+from urllib.parse import urlparse
+
+os.environ['MLFLOW_TRACKING_URI']=os.getenv("MLFLOW_TRACKING_URI")
+os.environ['MLFLOW_TRACKING_USERNAME']=os.getenv("MLFLOW_TRACKING_USERNAME") 
+os.environ["MLFLOW_TRACKING_PASSWORD"]=os.getenv("MLFLOW_TRACKING_PASSWORD")
+
+MLFLOW_TRACKING_URI = os.getenv("MLFLOW_TRACKING_URI")
+
+
+# Load parameters from params.yaml
+params = yaml.safe_load(open("params.yaml"))["train"]
+
+def evaluate(data_path,model_path):
+    data=pd.read_csv(data_path)
+    X = data.drop(columns=["Outcome"])
+    y = data["Outcome"]
+
+    mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
+
+    ## load the model from the disk
+    model=pickle.load(open(model_path, 'rb'))
+
+    predictions=model.predict(X)
+    accuracy=accuracy_score(y,predictions)
+    ## log metrics to MLFLOW
+
+    mlflow.log_metric("accuracy", accuracy)
+    print(f"Model accuracy: {accuracy}")
+
+if __name__=="__main__":
+    evaluate(params["data"], params["model"])
